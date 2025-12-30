14f794d78283b7ef879c4ba240bc545db3cb85d5
Axelhri <axelhouairi@gmail.com>
2025-12-30 03:28:42 +0100

    refactor: created star rating component

--- src/app/features/products/components/products-list.component.ts
+++ src/app/features/products/components/products-list.component.ts
@@ -1,43 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { ProductsFacade } from '../services/products.facade';
-import { bootstrapStar, bootstrapStarFill, bootstrapStarHalf } from '@ng-icons/bootstrap-icons';
-import { NgIcon, provideIcons } from '@ng-icons/core';
 import { CurrencyPipe, SlicePipe } from '@angular/common';
+import { StarRatingComponent } from '../../../star-rating/star-rating.component';
 
 @Component({
   selector: 'app-products-list',
   standalone: true,
-  providers: [
-    ProductsFacade,
-    provideIcons({
-      bootstrapStar,
-      bootstrapStarFill,
-      bootstrapStarHalf,
-    }),
-  ],
+  providers: [ProductsFacade],
   templateUrl: './products-list.component.html',
-  imports: [NgIcon, CurrencyPipe, SlicePipe],
+  imports: [StarRatingComponent, CurrencyPipe, SlicePipe],
 })
 export class ProductsListComponent {
   readonly facade = inject(ProductsFacade);
-
-  getStars(rating: number): ('full' | 'half' | 'empty')[] {
-    const stars: ('full' | 'half' | 'empty')[] = [];
-    const fullStars = Math.floor(rating);
-    const hasHalfStar = rating % 1 >= 0.5;
-
-    for (let i = 0; i < fullStars; i++) {
-      stars.push('full');
-    }
-
-    if (hasHalfStar) {
-      stars.push('half');
-    }
-
-    while (stars.length < 5) {
-      stars.push('empty');
-    }
-
-    return stars;
-  }
 }
---
+++ src/app/star-rating/star-rating.component.ts
@@ -0,0 +1,29 @@
+import { Component, computed, input } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { bootstrapStar, bootstrapStarFill, bootstrapStarHalf } from '@ng-icons/bootstrap-icons';
+
+@Component({
+  selector: 'app-star-rating',
+  imports: [NgIcon],
+  providers: [provideIcons({ bootstrapStar, bootstrapStarFill, bootstrapStarHalf })],
+  templateUrl: './star-rating.component.html',
+})
+export class StarRatingComponent {
+  rating = input.required<number>();
+
+  protected stars = computed(() => {
+    const val = this.rating();
+    const fullStars = Math.floor(val);
+    const hasHalfStar = val % 1 >= 0.5;
+
+    const stars: ('full' | 'half' | 'empty')[] = Array(fullStars).fill('full');
+
+    if (hasHalfStar) stars.push('half');
+
+    while (stars.length < 5) {
+      stars.push('empty');
+    }
+
+    return stars;
+  });
+}
