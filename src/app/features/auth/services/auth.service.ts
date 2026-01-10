88fc2cd277752479e46633f1ba87153badefb3c2
Axelhri <axelhouairi@gmail.com>
2026-01-10 16:19:29 +0100

    feat(auth): logout method

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { AuthenticationRequest } from '../models/authentication-request';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, pipe, tap } from 'rxjs';
 import { RegisterResponse } from '../models/register-response';
 
 @Injectable({
@@ -11,12 +11,25 @@ import { RegisterResponse } from '../models/register-response';
 export class AuthService {
   private readonly apiUrl = `${environment.apiUrl}auth/`;
   private readonly http = inject(HttpClient);
+  private loggedIn$ = new BehaviorSubject<boolean>(false);
 
   register(request: AuthenticationRequest): Observable<RegisterResponse> {
     return this.http.post<RegisterResponse>(`${this.apiUrl}register`, request);
   }
 
   login(request: AuthenticationRequest): Observable<string> {
-    return this.http.post<string>(`${this.apiUrl}login`, request, { withCredentials: true });
+    return this.http
+      .post<string>(`${this.apiUrl}login`, request, { withCredentials: true })
+      .pipe(tap(() => this.loggedIn$.next(true)));
+  }
+
+  logout(): Observable<void> {
+    return this.http
+      .post(`${this.apiUrl}logout`, {}, { withCredentials: true })
+      .pipe(tap(() => this.loggedIn$.next(false)));
+  }
+
+  isLoggedIn(): Observable<boolean> {
+    return this.loggedIn$.asObservable();
   }
 }
