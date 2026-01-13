9a27bb1f94cb91c6589802a1409bd154c263ffad
Axelhri <axelhouairi@gmail.com>
2026-01-13 15:42:26 +0100

    feat(cart): display cart total amount

--- src/app/features/cart/components/user-cart.component.ts
+++ src/app/features/cart/components/user-cart.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { CartProductsService } from '../services/cart-products.service';
 import { CartProducts } from '../models/cart-products';
 import { CurrencyPipe } from '../../../shared/pipes/currency.pipe';
+import { CartTotal } from '../models/cart-total';
 
 @Component({
   selector: 'app-user-cart',
@@ -12,6 +13,8 @@ import { CurrencyPipe } from '../../../shared/pipes/currency.pipe';
 export class UserCartComponent implements OnInit {
   private readonly cartProductsService = inject(CartProductsService);
   readonly products = signal<CartProducts[]>([]);
+  readonly totalAmount = signal<number>(0);
+
   readonly errorMessage = signal<string | null>(null);
 
   ngOnInit(): void {
@@ -21,6 +24,13 @@ export class UserCartComponent implements OnInit {
       },
       error: () => {
         this.errorMessage.set('An error occurred while fetching cart products.');
+      },
+    });
+
+    this.cartProductsService.getCartTotalAmount().subscribe({
+      next: (response) => {
+        this.totalAmount.set(response.total);
+        console.log(response);
       },
     });
   }
