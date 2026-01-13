7cf48d078902238116a55ef61529a7fe3da779aa
Axelhri <axelhouairi@gmail.com>
2026-01-13 15:27:15 +0100

    feat(cart): fetch cart products

---
+++ src/app/features/cart/services/cart-products.service.ts
@@ -0,0 +1,18 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CartProducts } from '../models/cart-products';
+import { ApiResponse } from '../../../shared/models/api-response';
+import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartProductsService {
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = `${environment.apiUrl}cart`;
+
+  getCartProducts(): Observable<ApiResponse<CartProducts[]>> {
+    return this.http.get<ApiResponse<CartProducts[]>>(this.apiUrl);
+  }
+}
