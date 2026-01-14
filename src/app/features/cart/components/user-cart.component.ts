1ab3740a857679164a79a29333e676b98d1b0483
Axelhri <axelhouairi@gmail.com>
2026-01-14 14:24:11 +0100

    feat(user-cart): increment cart product quantity

--- src/app/features/cart/components/user-cart.component.ts
+++ src/app/features/cart/components/user-cart.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { CartProductsService } from '../services/cart-products.service';
 import { CartProducts } from '../models/cart-products';
 import { CurrencyPipe } from '../../../shared/pipes/currency.pipe';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-user-cart',
@@ -13,18 +14,22 @@ export class UserCartComponent implements OnInit {
   private readonly cartProductsService = inject(CartProductsService);
   readonly products = signal<CartProducts[]>([]);
   readonly totalAmount = signal<number>(0);
+  readonly isLoading = signal<boolean>(false);
 
   readonly errorMessage = signal<string | null>(null);
 
   ngOnInit(): void {
-    this.cartProductsService.getCartProducts().subscribe({
-      next: (response) => {
-        this.products.set(response.data);
-      },
-      error: () => {
-        this.errorMessage.set('An error occurred while fetching cart products.');
-      },
-    });
+    this.cartProductsService
+      .getCartProducts()
+      .pipe(finalize(() => this.isLoading.set(false)))
+      .subscribe({
+        next: (response) => {
+          this.products.set(response.data);
+        },
+        error: () => {
+          this.errorMessage.set('An error occurred while fetching cart products.');
+        },
+      });
 
     this.cartProductsService.getCartTotalAmount().subscribe({
       next: (response) => {
@@ -32,5 +37,35 @@ export class UserCartComponent implements OnInit {
         console.log(response);
       },
     });
+  }
+
+  updateQuantity(productId: string, quantity: number): void {
+    this.isLoading.set(true);
+
+    this.cartProductsService
+      .addProductToCart({
+        product_id: productId,
+        product_quantity: Number(quantity),
+      })
+      .pipe(finalize(() => this.isLoading.set(false)))
+      .subscribe({
+        next: (res) => this.updateProductState(res.data),
+      });
+  }
+
+  private updateProductState(updatedItem: CartProducts): void {
+    this.products.update((items) => {
+      const index = items.findIndex((i) => i.product_id === updatedItem.product_id);
+
+      if (index === -1) {
+        return [...items, updatedItem];
+      }
+
+      return items.map((item) => (item.product_id === updatedItem.product_id ? updatedItem : item));
+    });
+
+    this.totalAmount.set(
+      this.products().reduce((sum, p) => sum + p.product_price * p.product_quantity, 0),
+    );
   }
 }
