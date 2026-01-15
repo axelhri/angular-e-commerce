62802be1115ae648f72ab34a6304106725cf7dd3
Axelhri <axelhouairi@gmail.com>
2026-01-15 12:24:25 +0100

    feat(checkout): stripe checkout service

---
+++ src/app/features/order/services/stripe.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { loadStripe, PaymentIntentResult, Stripe, StripeElements } from '@stripe/stripe-js';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StripeService {
+  private stripePromise = loadStripe(environment.stripeKey);
+
+  async getStripe(): Promise<Stripe> {
+    const stripe = await this.stripePromise;
+    if (!stripe) throw new Error('Stripe not loaded');
+    return stripe;
+  }
+
+  async confirmPayment(elements: StripeElements): Promise<PaymentIntentResult> {
+    const stripe = await this.getStripe();
+
+    return stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: 'http://localhost:4200',
+      },
+    });
+  }
+}
