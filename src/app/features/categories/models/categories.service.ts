872299bd1d630f84c194f149f793e595448c2317
Axelhri <axelhouairi@gmail.com>
2026-02-12 14:23:06 +0100

    feat(categories): category response model

---
+++ src/app/features/categories/models/categories.service.ts
@@ -0,0 +1,18 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ApiResponse } from '../../../shared/models/api-response';
+import { CategoryResponse } from './category-response';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookmarksService {
+  private readonly apiUrl = `${environment.apiUrl}categories`;
+  private readonly http = inject(HttpClient);
+
+  getCategories(): Observable<ApiResponse<CategoryResponse[]>> {
+    return this.http.get<ApiResponse<CategoryResponse[]>>(this.apiUrl);
+  }
+}
