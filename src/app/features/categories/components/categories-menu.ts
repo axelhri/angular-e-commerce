3a8f9f67d516dc86001b3804e1acb4d593bd33f3
Axelhri <axelhouairi@gmail.com>
2026-02-12 16:13:28 +0100

    feat(navbar): mobile categories menu interaction

--- src/app/features/categories/components/categories-menu.ts
+++ src/app/features/categories/components/categories-menu.ts
@@ -14,6 +14,22 @@ export class CategoriesMenu implements OnInit {
   private readonly categoriesService = inject(CategoriesService);
   readonly categories = signal<CategoryResponse[]>([]);
 
+  openedCategoryIds = new Set<string>();
+
+  toggleCategory(category: CategoryResponse): void {
+    if (category.children && category.children.length > 0) {
+      if (this.openedCategoryIds.has(category.category_id)) {
+        this.openedCategoryIds.delete(category.category_id);
+      } else {
+        this.openedCategoryIds.add(category.category_id);
+      }
+    }
+  }
+
+  isOpened(id: string): boolean {
+    return this.openedCategoryIds.has(id);
+  }
+
   ngOnInit(): void {
     this.categoriesService
       .getCategories()
