15c09f726b08a7e3e0fc9706b4d65a0a537081ef
Axelhri <axelhouairi@gmail.com>
2026-02-19 13:34:57 +0100

    feat(categories): redirect to products page with selected category

--- src/app/features/categories/components/categories-menu.ts
+++ src/app/features/categories/components/categories-menu.ts
@@ -5,6 +5,7 @@ import { finalize } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { bootstrapChevronDown } from '@ng-icons/bootstrap-icons';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-categories-menu',
@@ -15,23 +16,10 @@ import { bootstrapChevronDown } from '@ng-icons/bootstrap-icons';
 })
 export class CategoriesMenu implements OnInit {
   private readonly categoriesService = inject(CategoriesService);
+  private readonly router = inject(Router);
   readonly categories = signal<CategoryResponse[]>([]);
 
   openedCategoryIds = new Set<string>();
-
-  toggleCategory(category: CategoryResponse): void {
-    if (category.children && category.children.length > 0) {
-      if (this.openedCategoryIds.has(category.category_id)) {
-        this.openedCategoryIds.delete(category.category_id);
-      } else {
-        this.openedCategoryIds.add(category.category_id);
-      }
-    }
-  }
-
-  isOpened(id: string): boolean {
-    return this.openedCategoryIds.has(id);
-  }
 
   ngOnInit(): void {
     this.categoriesService
@@ -46,5 +34,28 @@ export class CategoriesMenu implements OnInit {
           console.log('error');
         },
       });
+  }
+
+  toggleCategory(category: CategoryResponse): void {
+    if (category.children && category.children.length > 0) {
+      if (this.openedCategoryIds.has(category.category_id)) {
+        this.openedCategoryIds.delete(category.category_id);
+      } else {
+        this.openedCategoryIds.add(category.category_id);
+      }
+      return;
+    }
+
+    this.router.navigate(['/products'], {
+      queryParams: {
+        categoryId: category.category_id,
+        page: 0,
+      },
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  isOpened(id: string): boolean {
+    return this.openedCategoryIds.has(id);
   }
 }
